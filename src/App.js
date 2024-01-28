b0065204465fdd6a3bd8bc02000458f8b7957166
David Adisa <adisad377@gmail.com>
2024-01-28 03:38:06 +0100

    worked with affinidi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React,{useState} from "react"
+import { AffinidiLoginButton, useAffinidiProfile } from '@affinidi/affinidi-react-auth'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { isLoading, error, profile, handleLogout } = useAffinidiProfile({
+    authCompleteUrl: '/api/affinidi-auth/complete'
+ })
+
+ async function logout() {
+  //clear session cookie
+  handleLogout();
+  window.location.href = "/";
+}
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {!profile && <>
+        <AffinidiLoginButton />
+      </>}
+
+      {isLoading && <p>Loading...</p>}
+
+      {profile && <>
+        <button style={{ marginRight: 10 }} onClick={logout}>
+          Logout
+        </button>
+
+        <h3>User Profile</h3>
+        <pre style={{ textAlign: "left" }}>{JSON.stringify(profile, null, 4)}</pre>
+      </>}
+
+      {error && <><h2>error</h2>{error}</>}
+
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
 }
 
 export default App;
